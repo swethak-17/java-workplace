75803cd4d8c936c3f5dbba6be63ff04af976bb80
swethak-17 <soulswetha1234@gmail.com>
2026-07-16 19:14:50 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,20 @@
+public class oddeven{
+    public static void main(String []args){
+int arr[]={27,20,4059,20,56};
+int odd=0;
+int even=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                even++;
+            }
+                else{
+                    odd++;
+                }}
+                System.out.println(" even values: "+even);
+                System.out.println(" odd values: "+odd);
+            }
+}
+            
+        
+            
+            
